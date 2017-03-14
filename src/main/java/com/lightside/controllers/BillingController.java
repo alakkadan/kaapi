a2be0cfb49939c3c613c7591093c20b3a562b72a
alakkadan <sunithaalakkadan@gmail.com>
2017-03-13 19:59:14 -0600

    brach1 created

---
+++ src/main/java/com/lightside/controllers/BillingController.java
@@ -0,0 +1,15 @@
+package com.lightside.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by sa on 03/13/17
+ -Learn git operation
+ */
+
+@RestController
+public class BillingController {
+
+    
+}
